976c7c922be5fff7078f61a737c8fa925c62f37b
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-06-29 02:38:22 +0100

    hot fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # this code installs flask
 
+package { 'Werkzeug':
+  ensure    => '2.1.1',
+  provider  => 'pip3',
+  require   => Package['Flask'],
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
