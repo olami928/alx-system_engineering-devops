f61b38f167c2c9131e526385b01aa60e6a7d6399
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-08-18 07:01:25 +0100

    fixed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,15 +5,15 @@ package { 'nginx':
   ensure => 'installed'
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-file_line { 'redirection-301':
+file_line { 'install':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://github.com/olami928 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
 }
 
 service { 'nginx':
